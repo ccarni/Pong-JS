a5dde6c39c6d8ec0ff8ac59991d24b63c1a220bc
ccarni <skylanderchance48@gmail.com>
2023-01-12 13:08:18 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 ﻿const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-const restartBtn = document.getElementById("restart");
-let gameLoopInterval;
 canvas.width = 1000;
 canvas.height = 500;
+const ctx = canvas.getContext("2d");
+const restartBtn = document.getElementById("restart");
 
 class Player {
     constructor(left, top, width, height, speed) {
@@ -13,13 +12,14 @@ class Player {
         this.height = height;
         this.speed = speed;
     }
-
+    
     update(input, _canvas) {
-       this.y -= input * this.speed;
-        // Clamps the paddle within the bounds of the canvas
-       this.y = Math.min(Math.max(this.y, 0), _canvas.height - this.height);
-    }
+        this.y -= input * this.speed;
 
+        // Clamps the paddle within the bounds of the canvas
+        this.y = Math.min(Math.max(this.y, 0), _canvas.height - this.height);
+    }
+    
     draw(_ctx) {
         _ctx.fillRect(this.x, this.y, this.width, this.height);
     }
@@ -32,77 +32,68 @@ class Ball {
         this.radius = radius;
         this.centerX = left + radius;
         this.centerY = top + radius;
-        this.originalSpeed = speed;
         this.speed = speed;
-
-        // Get a random velocity
-        this.v = [(Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2]
+        this.acceleration = speed/15
         
-        // Make the velocity a unit vector
-        const mag = Math.sqrt( Math.pow(this.v[0], 2) + Math.pow(this.v[1], 2) );
-        this.v[0] /= mag;
-        this.v[1] /= mag;
+        this.velocity = [(Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2]
         
-        // Speed up the velocity
-        this.v[0] *= speed;
-        this.v[1] *= speed; 
+        const velocityMagnitude = Math.sqrt( Math.pow(this.velocity[0], 2) + Math.pow(this.v[1], 2) );
+        this.velocity[0] /= velocityMagnitude;
+        this.velocity[1] /= velocityMagnitude;
+        
+        this.velocity[0] *= speed;
+        this.velocity[1] *= speed; 
     }
-
+    
     update(_canvas, playerOne, playerTwo) {
-        this.x += this.v[0];
-        this.y += this.v[1];
-
+        this.x += this.velocity[0];
+        this.y += this.velocity[1];
+        
         // Vertical collisions
         if (this.y < 0) {
             this.y = 0;
-            this.v[1] *= -1;
+            this.velocity[1] *= -1;
         }
-
+        
         if (this.y + this.radius*2 > _canvas.height) {
             this.y = _canvas.height - this.radius*2;
-            this.v[1] *= -1;
+            this.velocity[1] *= -1;
         }
-
+        
         // Player collisions
         if (this.x + this.radius*2 > playerTwo.x &&
             this.x + this.radius*2 < playerTwo.x + playerTwo.width &&
             this.y + this.radius*2 > playerTwo.y &&
             this.y + this.radius*2 < playerTwo.y + playerTwo.height) {
-            
+                
             this.x = playerTwo.x - this.radius*2;
-            this.centerX = this.x + this.radius;
-            this.v[0] *= -1;
-
-            this.v[0] *= 1 + this.speed/15;
-            this.v[1] *= 1 + this.speed/15;
-
+            this.velocity[0] *= -(1 + this.acceleration);
+            this.velocity[1] *= 1 + this.acceleration;
+                
         }
-
+            
         if (this.x > playerOne.x &&
             this.x < playerOne.x + playerOne.width &&
             this.y + this.radius*2 > playerOne.y &&
             this.y + this.radius*2 < playerOne.y + playerOne.height) {
-            
+                
             this.x = playerOne.x + playerOne.width;
-            this.centerX = this.x + this.radius;
-            this.v[0] *= -1;
-
-            this.v[0] *= 1 + this.speed/15;
-            this.v[1] *= 1 + this.speed/15;
+            this.velocity[0] *= -(1 + this.acceleration);
+            this.velocity[1] *= 1 + this.acceleration;
         }
-        
+                
         // Horizontal collisions
         if (this.x < 0) {
             return -1;
         }
-
+        
         if (this.x + this.radius*2 > canvas.width) {
             return 1;
         }
-
+                
         this.centerX = this.x + this.radius;
         this.centerY = this.y + this.radius;
-
+        
         return 0;
     }
     
@@ -113,67 +104,66 @@ class Ball {
         _ctx.fill();
         _ctx.closePath();
     }
-
-    center(_canvas) {
+    
+    resetAndCenter(_canvas) {
         this.x = _canvas.width / 2 - this.radius;
         this.y = _canvas.height / 2 - this.radius;
         this.centerX = this.x + this.radius;
         this.centerY = this.y + this.radius;
-        this.speed = this.originalSpeed;
-
-        // Get a random velocity
-        this.v = [(Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2]
         
-        // Make the velocity a unit vector
-        const mag = Math.sqrt( Math.pow(this.v[0], 2) + Math.pow(this.v[1], 2) );
-        this.v[0] /= mag;
-        this.v[1] /= mag;
+        this.velocity = [(Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2]
         
-        // Speed up the velocity
-        this.v[0] *= this.speed;
-        this.v[1] *= this.speed;
+        const velocityMagnitude = Math.sqrt( Math.pow(this.velocity[0], 2) + Math.pow(this.v[1], 2) );
+        this.velocity[0] /= velocityMagnitude;
+        this.velocity[1] /= velocityMagnitude;
+        
+        this.velocity[0] *= this.speed;
+        this.velocity[1] *= this.speed;
     }
 }
 
 class Game {
     constructor() {
-        // create the players
         this.playerOne = new Player(50, canvas.height / 2 - 75, 20, 150, 5);
         this.playerTwo = new Player(930, canvas.height / 2 - 75,
         20, 150, 5);
         this.playerOneScore = 0;
         this.playerTwoScore = 0;
-
         this.ball = new Ball(canvas.width / 2 - 25, canvas.height / 2 - 25, 20, 5);
-
-        this.isRunning = true;
-
-        this.wsInput = 0;
-        this.upDownInput = 0;
-
+        
+        this.playerOneInput = 0;
+        this.playerTwoInput = 0;
+        window.addEventListener('keyup', (e) => {
+            this.onKeyUp(e);
+        });
+        window.addEventListener('keydown', (e) => {
+            this.onKeyDown(e);
+        });
     }
     
     update() {
-        const status = this.ball.update(canvas, this.playerOne, this.playerTwo);
-
-        if (status == -1) {
+        const ballScoreStatus = this.ball.update(canvas, this.playerOne, this.playerTwo);
+        
+        if (ballScoreStatus == -1) {
             this.playerTwoScore++;
-            this.ball.center(canvas);
+            this.ball.resetAndCenter(canvas);
         }
-
-        if (status == 1) {
+        
+        if (ballScoreStatus == 1) {
             this.playerOneScore++;
-            this.ball.center(canvas);
+            this.ball.resetAndCenter(canvas);
         }
-
-        this.playerOne.update(this.wsInput, canvas);
-        this.playerTwo.update(this.upDownInput, canvas);
+        
+        this.playerOne.update(this.playerOneInput, canvas);
+        this.playerTwo.update(this.playerTwoInput, canvas);
     }
     
     draw() {
+        // Empty black canvas
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = "white";
+
         // Draw a line across the center
         ctx.beginPath();
         ctx.strokeStyle = "white";
@@ -182,13 +172,14 @@ class Game {
         ctx.stroke();
         ctx.closePath();
         
+        // Draw the scores
         ctx.fillStyle = "white";
         ctx.font = "48px sans-serif"
         ctx.fillText(this.playerOneScore.toString(), canvas.width / 2 - 48, 50);
         ctx.fillText(this.playerTwoScore.toString(), canvas.width / 2 + 24, 50);
-
+        
         this.ball.draw(ctx);
-        // Draw the players
+
         if (this.playerOneScore > this.playerTwoScore) {
             ctx.fillStyle = "gold";
         }
@@ -199,49 +190,43 @@ class Game {
             ctx.fillStyle = "gold";
         }
         this.playerTwo.draw(ctx);
-
+        
     }
     
     onKeyDown(e) {
         if (e.key == "w"){
-            this.wsInput = 1;
+            this.playerOneInput = 1;
         }
         if (e.key == "s") {
-            this.wsInput = -1;
+            this.playerOneInput = -1;
         }
         if (e.key == "ArrowUp") {
-            this.upDownInput = 1;
+            this.playerTwoInput = 1;
         }
         if (e.key == "ArrowDown") {
-            this.upDownInput = -1;
+            this.playerTwoInput = -1;
         }
     }
     
     onKeyUp(e) {
-        if (e.key == "w" && this.wsInput > 0){
-            this.wsInput = 0;
+        if (e.key == "w" && this.playerOneInput > 0){
+            this.playerOneInput = 0;
         }
-        if (e.key == "s" && this.wsInput < 0) {
-            this.wsInput = 0;
+        if (e.key == "s" && this.playerOneInput < 0) {
+            this.playerOneInput = 0;
         }
-        if (e.key == "ArrowUp" && this.upDownInput > 0) {
-            this.upDownInput = 0;
+        if (e.key == "ArrowUp" && this.playerTwoInput > 0) {
+            this.playerTwoInput = 0;
         }
-        if (e.key == "ArrowDown" && this.upDownInput < 0) {
-            this.upDownInput = 0;
+        if (e.key == "ArrowDown" && this.playerTwoInput < 0) {
+            this.playerTwoInput = 0;
         }
     }
-
+    
 }
 
 function main() {
     const game = new Game();
-    window.addEventListener('keyup', (e) => {
-        game.onKeyUp(e);
-    });
-    window.addEventListener('keydown', (e) => {
-        game.onKeyDown(e);
-    });
 
     gameLoopInterval = setInterval(() => {
         game.update();
@@ -249,9 +234,10 @@ function main() {
     }, 17);
 }
 
-main();
-
+let gameLoopInterval;
 restartBtn.addEventListener('click', () => {
     clearInterval(gameLoopInterval);
     main();
 });
+
+main();
